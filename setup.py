7037c88d698b05424d4f44b022305885104d4dad
Russell McCreath <russell.mccreath@phs.scot>
2021-08-16 14:30:59 +0100

    add attributes to setup function

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Import functions
+from setuptools import setup, find_packages
+
+# Setup
+setup(
+    author="Russell McCreath",
+    description="Functions to extract and interact with data from the Scottish Health and Social Care Open Data "
+                "platform.",
+    name="phsopendata",
+    version="0.0.1",
+    packages=find_packages(include=["phsopendata", "phsopendata.*"]),
+    install_requires=["re", "sys", "requests", "pandas", "io", "pytest"]
+)
